a24aa384a5dffc6ed2577382bc92950935e44090
shylock <tcath2s@gmail.com>
2018-03-04 18:56:04 +0800

    add parameter count check

--- mkpost.sh
+++ mkpost.sh
@@ -6,5 +6,9 @@ today=`date +%Y-%m-%d`
 #echo $today
 
 #creat post
-cp -f template.md _posts/$today-$1.md
-
+if [ $# -gt 0 ];
+then
+	cp -f template.md _posts/$today-$1.md
+else
+	echo "invalid count of parameters!"
+fi
