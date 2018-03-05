23acabf2fcee35c5564b6ce5a559ea9fc6b3fb72
shylock <tcath2s@gmail.com>
2018-03-05 22:15:11 +0800

    use getopts

--- mkpost.sh
+++ mkpost.sh
@@ -6,9 +6,23 @@ today=`date +%Y-%m-%d`
 #echo $today
 
 #creat post
-if [ $# -gt 0 ];
-then
-	cp -f template.md _posts/$today-$1.md
-else
-	echo "invalid count of parameters!"
-fi
+#if [ $# -gt 0 ]
+#then
+#	cp -f template.md _posts/$today-$1.md
+#else
+#	echo "invalid count of parameters!"
+#fi
+
+while getopts 'n:' args
+do
+	case $args in
+		n)
+			cp -f template.md _posts/$today-$OPTARG.md
+			;;
+		?)
+			echo "Unknown argument $OPTARG!"
+			;;
+	esac
+done
+
+shift $(($OPTIND - 1))
